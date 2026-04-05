8ec3830e36181324091e8fe30eac6eef4e14968e
Georgios Pegiazis <153769654+GeorgePeg@users.noreply.github.com>
2026-04-05 21:32:59 +0300

    Update copyright link in Ares_ver_1_2_3.ino

--- Ares_codes/Ares_ver_1_2_3.ino
+++ Ares_codes/Ares_ver_1_2_3.ino
@@ -1,6 +1,6 @@
 /*
   Name: Ares Mini Sumo Robot
-  Copyright: ©UoP Robotics Team (https://github.com/esdaLabWro)
+  Copyright: ©UoP Robotics Team (https://github.com/UoP-Robotics)
   Authors: Katerina Tertimpa (https://github.com/katerina-kt), Georgios Pegiazis (https://github.com/GeorgePeg), Ornela-Maria Qoshi (https://github.com/ornela-maria)
   Date: 29/03/26 21:40
   Version: 1.2.3
